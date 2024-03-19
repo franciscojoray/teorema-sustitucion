8bba96734c12d83799823eb8643c0d260ac70653
Francisco Joray <franjoray@gmail.com>
2024-03-18 22:25:17 -0300

    added sigma_bottom & substitutions def.

--- Sustitucion.thy
+++ Sustitucion.thy
@@ -4,6 +4,9 @@ begin
 
 type_synonym var = string
 type_synonym \<Sigma> = "var \<Rightarrow> int"
+type_synonym \<Delta> = "var \<Rightarrow> var"
+
+datatype \<Sigma>b = Norm \<Sigma> | Bottom
 
 datatype intexp =
   Natconst nat
@@ -14,6 +17,16 @@ datatype intexp =
   | Div intexp intexp
   | Mod intexp intexp
   | Prod intexp intexp
+
+fun subint :: "intexp \<Rightarrow> \<Delta> \<Rightarrow> intexp" where
+  "subint (Natconst n) _ = Natconst n"
+| "subint (Var v) \<delta> = Var (\<delta> v)"
+| "subint (Neg e) \<delta> = Neg (subint e \<delta>)"
+| "subint (Sum e0 e1) \<delta> = Sum (subint e0 \<delta>) (subint e1 \<delta>)"
+| "subint (Sub e0 e1) \<delta> = Sub (subint e0 \<delta>) (subint e1 \<delta>)"
+| "subint (Div e0 e1) \<delta> = Div (subint e0 \<delta>) (subint e1 \<delta>)"
+| "subint (Mod e0 e1) \<delta> = Mod (subint e0 \<delta>) (subint e1 \<delta>)"
+| "subint (Prod e0 e1) \<delta> = Prod (subint e0 \<delta>) (subint e1 \<delta>)"
 
 fun semint :: "intexp \<Rightarrow> \<Sigma> \<Rightarrow> int" where
   "semint (Natconst n) \<sigma> = int n"
@@ -55,14 +68,32 @@ datatype comm =
   | Newvar var intexp comm
   (*| While boolexp comm*)
 
-fun semcomm :: "comm \<Rightarrow> \<Sigma> \<Rightarrow> \<Sigma>" where
-  "semcomm Skip \<sigma> = \<sigma>"
-| "semcomm (Assign v e) \<sigma> = (\<lambda> x. if x = v then (semint e \<sigma>) else \<sigma> v)"
-| "semcomm (Seq c0 c1) \<sigma> = semcomm c1 (semcomm c0 \<sigma>)"
+fun star :: "(\<Sigma> \<Rightarrow> \<Sigma>b) \<Rightarrow> (\<Sigma>b \<Rightarrow> \<Sigma>b)" where
+  "star f (Norm \<sigma>) = f \<sigma>"
+| "star f Bottom = Bottom"
+
+fun semcomm :: "comm \<Rightarrow> \<Sigma> \<Rightarrow> \<Sigma>b" where
+  "semcomm Skip \<sigma> = Norm \<sigma>"
+| "semcomm (Assign v e) \<sigma> = Norm (\<lambda> x. if x = v then (semint e \<sigma>) else \<sigma> v)"
+| "semcomm (Seq c0 c1) \<sigma> = star (semcomm c1) (semcomm c0 \<sigma>)"
 | "semcomm (Cond b c0 c1) \<sigma> = 
     (if (sembool b \<sigma>) then (semcomm c0 \<sigma>) else (semcomm c1 \<sigma>))"
 | "semcomm (Newvar v e c) \<sigma> =
-    (\<lambda> \<sigma>'. (\<lambda> x. if x = v then \<sigma> v else \<sigma>' x))
+    star (\<lambda> \<sigma>'. Norm (\<lambda> x. if x = v then \<sigma> v else \<sigma>' x))
     (semcomm c (\<lambda> x. if x = v then (semint e \<sigma>) else \<sigma> x))"
+
+fun FA :: "comm \<Rightarrow> var set" where
+  "FA Skip = {}"
+| "FA (Assign v e) = {v}"
+| "FA (Seq c0 c1) = FA c0 \<union> FA c1"
+| "FA (Cond b c0 c1) = FA c0 \<union> FA c1"
+| "FA (Newvar v e c) = FA c - {v}"
+
+fun subcomm :: "comm \<Rightarrow> \<Delta> \<Rightarrow> comm" where
+  "subcomm Skip _ = Skip"
+| "subcomm (Assign v e) \<delta> = Assign (\<delta> v) (subint e \<delta>)"
+| "subcomm (Seq c0 c1) \<delta> = Seq (subcomm c0 \<delta>) (subcomm c1 \<delta>)"
+| "subcomm (Cond b c0 c1) \<delta> = Cond b (subcomm c0 \<delta>) (subcomm c1 \<delta>)"
+(*| "subcomm (Newvar v e c) = ?"*)
 
 end
